08e596d38b932f400187af3712cb017328bc24cc
zhangruyi3906 <3023208132@qq.com>
2024-05-19 16:26:27 +0800

    update 第五次

--- src/main/java/com/yupi/usercenter/controller/UserController.java
+++ src/main/java/com/yupi/usercenter/controller/UserController.java
@@ -31,72 +31,88 @@ public class UserController {
 
     @PostMapping("/register")
     public Long userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
-        if(userRegisterRequest==null){
+        if (userRegisterRequest == null) {
             return null;
         }
         String userAccount = userRegisterRequest.getUserAccount();
         String userPassword = userRegisterRequest.getUserPassword();
         String checkPassword = userRegisterRequest.getCheckPassword();
-        if(StringUtils.isAllBlank(userAccount,userPassword,checkPassword)){
+        if (StringUtils.isAllBlank(userAccount, userPassword, checkPassword)) {
             return null;
         }
         return userService.userRegister(userAccount, userPassword, checkPassword);
 
     }
+
     @PostMapping("/login")
     public User userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
-        if(userLoginRequest==null){
+        if (userLoginRequest == null) {
             return null;
         }
         String userAccount = userLoginRequest.getUserAccount();
         String userPassword = userLoginRequest.getUserPassword();
-        if(StringUtils.isAllBlank(userAccount,userPassword)){
+        if (StringUtils.isAllBlank(userAccount, userPassword)) {
             return null;
         }
-        return userService.userLogin(userAccount, userPassword,request);
+        return userService.userLogin(userAccount, userPassword, request);
 
     }
 
+    @GetMapping("/current")
+    public User getCurrentUser(HttpServletRequest request) {
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null) {
+            return null;
+        }
+        Long userId = currentUser.getId();
+//        TODO 校验用户是否合法
+        User user=userService.getById(userId);
+        return userService.getSafetyUser(user);
+    }
+
+
     @GetMapping("/search")
-    public List<User> searchUsers(String username,HttpServletRequest request){
+    public List<User> searchUsers(String username, HttpServletRequest request) {
         //仅管理员可查询
 
-        if(!isAdmin(request)){
+        if (!isAdmin(request)) {
             return new ArrayList<>();
         }
 
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-        if(StringUtils.isNotBlank(username)){
-            queryWrapper.like("username",username);
+        if (StringUtils.isNotBlank(username)) {
+            queryWrapper.like("username", username);
         }
-        List<User> userList=userService.list(queryWrapper);
+        List<User> userList = userService.list(queryWrapper);
         return userList.stream().map(user -> userService.getSafetyUser(user)).collect(Collectors.toList());
     }
 
     @PostMapping("/delete")
-    public boolean deleteUser(@RequestBody long id,HttpServletRequest request){
+    public boolean deleteUser(@RequestBody long id, HttpServletRequest request) {
 
-        if(!isAdmin(request)){
+        if (!isAdmin(request)) {
             return false;
         }
-        if(id<=0){
+        if (id <= 0) {
             return false;
         }
-       return userService.removeById(id);
+        return userService.removeById(id);
 
     }
 
     /**
      * 是否为管理员
+     *
      * @param request
      * @return
      */
-    private boolean isAdmin(HttpServletRequest request){
+    private boolean isAdmin(HttpServletRequest request) {
         //仅管理员可查询
         Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
-        User user = (User)userObj;
+        User user = (User) userObj;
 
-        return user==null||user.getUserRole()!=ADMIN_ROLE;
+        return user == null || user.getUserRole() != ADMIN_ROLE;
 
     }
 }
